29845614efc4d5f9e36d913ba99a8749b235c919
nitzanto <nitzan16001@gmail.com>
2023-12-01 21:29:27 +0200

    * Data updated

--- src/data/index.js
+++ src/data/index.js
@@ -1,21 +1,32 @@
+import {
+  finnAndJake,
+  rickAndMorty,
+  spongebobAndPatrick,
+} from "../assets/images/index.js";
+
 const restaurantsData = [
   {
     id: "1",
-    name: "Ocean Delight",
-    description: "A seafood paradise by the shore.",
+    name: "Krusty Krab 🦀",
+    description:
+      "Explore the maritime flavors of Krusty Krab's American-style cuisine through Bookify – a delectable journey of seafood, burgers, and classic comforts.",
+    imgURL: spongebobAndPatrick,
     location: "123 Ocean Avenue",
     cuisine: "Seafood",
   },
   {
     id: "2",
-    name: "Taste of Italy",
-    description: "Authentic Italian flavors in every bite.",
+    name: "Szechuan Sushi Galaxy 🍣",
+    imgURL: rickAndMorty,
+    description:
+      "Embark on an interdimensional sushi adventure at Szechuan Sushi Galaxy, where each roll is a cosmic blend of traditional Asian flavors, served with a side of multiversal charm.",
     location: "456 Olive Street",
     cuisine: "Italian",
   },
   {
     id: "3",
-    name: "Spicy Bites",
+    name: "Pasta Enchantment 🍝",
+    imgURL: finnAndJake,
     description: "Experience the bold flavors of spicy cuisine.",
     location: "789 Pepper Plaza",
     cuisine: "Spicy",
