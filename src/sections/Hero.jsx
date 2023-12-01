5e658650a3063b6c61084e71a175a4dec511d297
nitzanto <nitzan16001@gmail.com>
2023-12-01 17:39:11 +0200

    * Proper naming

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
           src={bookify3d}
-          alt="shoe colletion"
+          alt="Bookify Logo 3D"
           width={368}
           height={368}
           className="object-contain relative z-10"
