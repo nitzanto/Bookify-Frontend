112370a4a54c3703ba49cb500850d1370484d0f3
nitzanto <nitzan16001@gmail.com>
2023-12-01 20:35:18 +0200

    * Datasource created

---
+++ src/data/index.js
@@ -0,0 +1,25 @@
+const restaurantsData = [
+  {
+    id: "1",
+    name: "Ocean Delight",
+    description: "A seafood paradise by the shore.",
+    location: "123 Ocean Avenue",
+    cuisine: "Seafood",
+  },
+  {
+    id: "2",
+    name: "Taste of Italy",
+    description: "Authentic Italian flavors in every bite.",
+    location: "456 Olive Street",
+    cuisine: "Italian",
+  },
+  {
+    id: "3",
+    name: "Spicy Bites",
+    description: "Experience the bold flavors of spicy cuisine.",
+    location: "789 Pepper Plaza",
+    cuisine: "Spicy",
+  },
+];
+
+export default restaurantsData;
