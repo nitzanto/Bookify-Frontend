2caa7140de31b68e78950e27ae879646f350f427
nitzanto <nitzan16001@gmail.com>
2023-12-01 20:20:07 +0200

    * Styling fixes

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,4 @@
-import arrowRight from "./arrow-right.svg";
+import arrowRight from "./arrow-right.png";
 import chevronLeft from "./chevron-left.svg";
 import cheveronRight from "./chevron-right.svg";
 import copyrightSign from "./copyright-sign.svg";
