fedc241ea09d00a90629603edbfa34ea1f15aee1
nitzanto <nitzan16001@gmail.com>
2023-12-03 15:01:20 +0200

    * Services created

---
+++ src/libs/common/constants/services.js
@@ -0,0 +1,10 @@
+export const AUTH_SERVICE_LOGIN = `${
+  import.meta.env.VITE_BOOKIFY_API
+}/auth/login`;
+export const AUTH_SERVICE_LOGOUT = `${
+  import.meta.env.VITE_BOOKIFY_API
+}/auth/logout`;
+export const RESERVATIONS_SERVICE = `${
+  import.meta.env.VITE_BOOKIFY_API
+}/reservations/`;
+export const USER_SERVICE = `${import.meta.env.VITE_BOOKIFY_API}/users/`;
