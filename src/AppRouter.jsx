9b64324035071db386250838856948ad290cc6eb
nitzanto <nitzan16001@gmail.com>
2023-12-03 16:40:22 +0200

    * Sign up page added

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RestaurantPage from "./components/RestaurantPage";
 import App from "./App.jsx";
-import { LoginPage } from "./components/index.js";
+import { LoginPage, SignUp } from "./components/index.js";
+
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<App />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/restaurant/:id" element={<RestaurantPage />} />
       </Routes>
     </Router>
