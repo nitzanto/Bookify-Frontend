371bf4e6121a0c9a033c82cf4e086b8cb29bbe09
nitzanto <nitzan16001@gmail.com>
2023-12-05 15:20:21 +0200

    * Cleaned the code

--- src/libs/common/components/Nav.jsx
+++ src/libs/common/components/Nav.jsx
@@ -30,18 +30,20 @@ const Nav = () => {
             className="m-0 w-[180px] h-[180px]"
           />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
-          {navLinks.map((item) => (
-            <li key={item.label}>
-              <a
-                href={item.href}
-                className="font-montserrat leading-normal text-lg text-slate-gray"
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
-        </ul>
+        {isMainPage && (
+          <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+            {navLinks.map((item) => (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
         <div className="flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24">
           <a href="/">
             <img src={user} width={25} height={25} alt="user icon" />
